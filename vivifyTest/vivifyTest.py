e5e0734288f1715c897c59a12040bd0926371345
Djole <djordje.stojanovic@ucircuit.com>
2021-03-19 22:18:07 +0100

    User registration

--- vivifyTest/vivifyTest.py
+++ vivifyTest/vivifyTest.py
@@ -21,3 +21,29 @@ class TestGalleryApp:
         sleep(1)
 
         assert (driver.current_url == "https://gallery-app.vivifyideas.com/")
+
+    # go to the register page
+    def test_2(self):
+        driver.find_element_by_xpath("//a[contains(text(),'Register')]").click()
+        sleep(0.5)
+
+        assert (driver.current_url == "https://gallery-app.vivifyideas.com/register")
+
+    # user registration
+    def test_3(self):
+        driver.find_element_by_xpath("//input[@id='first-name']").send_keys("test1")
+        sleep(.2)
+        driver.find_element_by_xpath("//input[@id='last-name']").send_keys("test1")
+        sleep(.2)
+        driver.find_element_by_xpath("//input[@id='email']").send_keys("test12@gmail.com")
+        sleep(.2)
+        driver.find_element_by_xpath("//input[@id='password']").send_keys("12345678")
+        sleep(.2)
+        driver.find_element_by_xpath("//input[@id='password-confirmation']").send_keys("12345678")
+        sleep(.2)
+        driver.find_element_by_xpath("//body/div[@id='app']/div[2]/div[1]/form[1]/div[6]/input[1]").click()
+        sleep(.2)
+        driver.find_element_by_xpath("//button[contains(text(),'Submit')]").click()
+        sleep(1)
+
+        assert (driver.find_element_by_xpath("//a[contains(text(),'Logout')]").text == "Logout")
